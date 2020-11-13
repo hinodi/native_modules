84f64b56f9a888064edbf6fd6fa16a6c46a7580e
hinodi <huynhngocdinh98@gmail.com>
2020-11-13 19:04:24 +0700

    basic animation

--- App.js
+++ App.js
@@ -1,19 +1,71 @@
 import React, {Component} from 'react';
-import {StyleSheet, Text, View, NativeModules} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  NativeModules,
+  TouchableOpacity,
+  LayoutAnimation,
+  Easing,
+  Animated,
+} from 'react-native';
+
+const {UIManager} = NativeModules;
+
+UIManager.setLayoutAnimationEnabledExperimental &&
+  UIManager.setLayoutAnimationEnabledExperimental(true);
 
 export default class App extends Component {
+  state = {
+    value: new Animated.Value(0),
+  };
+
+  _onPress = () => {
+    Animated.timing(this.state.value, {
+      toValue: 1,
+      duration: 2000,
+      // useNativeDriver: true,
+    }).start();
+  };
+  _onPress2 = () => {
+    Animated.timing(this.state.value, {
+      toValue: 0,
+      duration: 2000,
+      // useNativeDriver: true,
+    }).start();
+  };
   componentDidMount() {
-    NativeModules.MyModule.addWithCallback(1, 2, (res) => {
-      console.log(res);
-    });
-    NativeModules.MyModule.addWithPromise(1, 2)
-      .then((res) => console.log(res))
-      .catch((err) => console.log({...err}));
+    // NativeModules.MyModule.addWithCallback(1, 2, (res) => {
+    //   console.log(res);
+    // });
+    // NativeModules.MyModule.addWithPromise(1, 2)
+    //   .then((res) => console.log(res))
+    //   .catch((err) => console.log({...err}));
   }
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Animated.View
+          style={[
+            styles.box,
+            {
+              marginBottom: this.state.value.interpolate({
+                inputRange: [0, 0.5, 1],
+                outputRange: [100, 50, 250],
+              }),
+            },
+          ]}
+        />
+        <TouchableOpacity onPress={this._onPress}>
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>Press me!</Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={this._onPress2}>
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>Press me!</Text>
+          </View>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -22,13 +74,22 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: 'center',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  box: {
+    width: 200,
+    height: 200,
+    backgroundColor: 'red',
+  },
+  button: {
+    backgroundColor: 'black',
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+    marginTop: 15,
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
   },
 });
